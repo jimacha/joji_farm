9cf49834e09aa49bf9d8fb75ea680a10c01a94f7
jimacha <machariajimmy68@gmail.com>
2023-09-04 21:39:12 +0300

    update

--- app/models/console.py
+++ app/models/console.py
@@ -1,84 +1,29 @@
 import sqlite3
+import os
 
 class Console:
     def __init__(self):
-        self.conn = sqlite3.connect('users.db')
-        self.create_tables()
+        db_file = 'users.db'
+        new_db = not os.path.isfile(db_file)
+
+        self.conn = sqlite3.connect(db_file)
+
+        if new_db:
+            self.create_tables()
+
+    # ... (rest of your Console class methods)
 
     def create_tables(self):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                phone_number TEXT,
-                email TEXT,
-                password TEXT,
-                confirm_password TEXT
-            )
-        ''')
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS farms (
-                id INTEGER PRIMARY KEY,
-                user_id INTEGER,
-                crop_type TEXT,
-                crop_size INTEGER,
-                FOREIGN KEY (user_id) REFERENCES users (id)
-            )
-        ''')
-        self.conn.commit()
+        # Your table creation code here
 
-    def add_farm(self, user_id, crop_type, crop_size):
-        cursor = self.conn.cursor()
-        cursor.execute('INSERT INTO farms (user_id, crop_type, crop_size) VALUES (?, ?, ?)', (user_id, crop_type, crop_size))
-        self.conn.commit()
-        return True
-
-    def create_user(self, name, phone_number, email, password, confirm_password):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            INSERT INTO users (name, phone_number, email, password, confirm_password)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (name, phone_number, email, password, confirm_password))
-        self.conn.commit()
-        return cursor.lastrowid
-
-    def get_user(self, user_id):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
-        user_data = cursor.fetchone()
-
-        if user_data:
-            cursor.execute('SELECT crop_type, crop_size FROM farms WHERE user_id = ?', (user_id,))
-            farms = cursor.fetchall()
-            user_data = dict(user_data)
-            user_data['farms'] = [{'crop_type': farm[0], 'crop_size': farm[1]} for farm in farms]
-
-        return user_data
-
-    def get_all_users(self):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM users')
-        users_data = cursor.fetchall()
-        users = []
-
-        for user_data in users_data:
-            user_id = user_data[0]
-            cursor.execute('SELECT crop_type, crop_size FROM farms WHERE user_id = ?', (user_id,))
-            farms = cursor.fetchall()
-            user_data = dict(user_data)
-            user_data['farms'] = [{'crop_type': farm[0], 'crop_size': farm[1]} for farm in farms]
-            users.append(user_data)
-
-        return users
+    # ... (rest of your Console class methods)
 
     def close_connection(self):
         self.conn.close()
 
-# Example usage:
 if __name__ == '__main__':
     console = Console()
-    user_id = console.create_user('Jimmy Macharia', '1234567890', 'macharia.com', 'password123', 'password123')
+    user_id = console.create_user('John Doe', '1234567890', 'john@example.com', 'password123', 'password123')
     console.add_farm(user_id, 'Wheat', 10)
     console.add_farm(user_id, 'Corn', 8)
 
@@ -91,3 +36,4 @@ if __name__ == '__main__':
     print(all_users)
 
     console.close_connection()
+
